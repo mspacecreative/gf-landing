58f525b6b81405beb1747dc969d1bc028fee45ae
Matt <matt@mspacecreative.com>
2021-03-04 14:27:15 -0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,6 +108,22 @@
 				slidesToScroll: 3,
 				infinite: false,
 				appendArrows: $('.append-arrows'),
+				responsive: [
+					{
+						breakpoint: 768,
+						settings: {
+					        slidesToShow: 2,
+							slidesToScroll: 2
+						}
+				    },
+					{
+						breakpoint: 480,
+						settings: {
+					        slidesToShow: 1,
+							slidesToScroll: 1
+						}
+				    }
+				]
 			});
 			
 			/*
